449e5a9454813a9bb7f12662d3926758857f48c0
Vassil Keremidchiev <varosi@gmail.com>
2019-02-25 01:08:48 +0200

    stylish-haskell run

--- src/System/Hardware/Robko01.hs
+++ src/System/Hardware/Robko01.hs
@@ -1,28 +1,30 @@
-{-# LANGUAGE OverloadedStrings, ScopedTypeVariables, ApplicativeDo #-}
-module System.Hardware.Robko01( 
+{-# LANGUAGE ApplicativeDo       #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+module System.Hardware.Robko01(
     Robko01,
-    Joint(..), JointSteps(..), 
-    initRobko, doSomething, 
+    Joint(..), JointSteps(..),
+    initRobko, doSomething,
     resetAndStop, getJointSteps, getInputStatus,
     cmdString ) where    -- Exported because of tests
 
-import Data.List.NonEmpty
-import qualified Data.ByteString.Char8 as B
-import Text.Printf                              (printf)
-import System.Hardware.Serialport
-import Control.Concurrent                       (threadDelay)
-import Data.Attoparsec.ByteString
-import Data.Attoparsec.ByteString.Char8         (char, digit)
-import Control.Applicative
-import Control.Monad.IO.Class
+import           Control.Applicative
+import           Control.Concurrent               (threadDelay)
+import           Control.Monad.IO.Class
+import           Data.Attoparsec.ByteString
+import           Data.Attoparsec.ByteString.Char8 (char, digit)
+import qualified Data.ByteString.Char8            as B
+import           Data.List.NonEmpty
+import           System.Hardware.Serialport
+import           Text.Printf                      (printf)
 
-data Joint = MotorBase | Motor1 | Motor2 | Motor3 | Motor4 | Motor5 | Aux0 | Aux1 
+data Joint = MotorBase | Motor1 | Motor2 | Motor3 | Motor4 | Motor5 | Aux0 | Aux1
     deriving (Show, Enum)
 
 data DriveDir  = Down | Up           deriving (Show, Enum)
 data DriveMode = FullStep | HalfStep deriving (Show, Enum)
 
-newtype JointSteps = JointSteps Int 
+newtype JointSteps = JointSteps Int
     deriving (Show)
 
 -- | Robko01 monad for its commands
@@ -31,14 +33,14 @@ newtype Robko01 a = Robko01 { runRobko' :: SerialPort -> Int -> IO a }
 instance Functor Robko01 where
     fmap f (Robko01 a) = Robko01 (\port deviceId -> do
         x <- a port deviceId
-        return $ f x)
+        pure $ f x)
 
 instance Applicative Robko01 where
-    pure a = Robko01 (\_ _ -> pure a)
+    pure a = Robko01 $ \_ _ -> pure a
     (Robko01 f) <*> (Robko01 a') = Robko01 (\port deviceId -> do
         g <- f port deviceId
         a <- a' port deviceId
-        return $ g a)
+        pure $ g a)
 
 instance Monad Robko01 where
     return   = pure
@@ -52,7 +54,7 @@ instance MonadIO Robko01 where
 
 -- | Run Robko01 program
 runRobko :: String -> Int -> Robko01 a -> IO a
-runRobko port deviceId commands = 
+runRobko port deviceId commands =
     withSerial port portSettings $ \s -> do
         initRobko s
         runRobko' commands s deviceId
@@ -67,10 +69,10 @@ parseBit = do
 port = "/dev/tty.ROBKO01-RNI-SPP"  -- MacOS
 
 portSettings = defaultSerialSettings { commSpeed = CS19200, timeout = 50 }
-cmdTime = 200000 -- 200ms for Robko 01 to 
+cmdTime = 200000 -- 200ms for Robko 01 to
 
 initRobko :: SerialPort -> IO ()
-initRobko s = do 
+initRobko s = do
     -- Wait to init and read initialization
     threadDelay cmdTime
     helloRobko <- recv s 255
@@ -82,21 +84,21 @@ cmdString :: Int -> Int -> Int -> Int -> Int -> Int -> B.ByteString
 cmdString deviceId cmd arg0 arg1 arg2 arg3 = B.pack $ printf ":%02d%02d%d%d%04d%04d\r\n" deviceId cmd arg0 arg1 arg2 arg3
 
 cmdRobko :: Int -> Int -> Int -> Int -> Int -> Robko01 (B.ByteString, B.ByteString)
-cmdRobko cmd arg0 arg1 arg2 arg3 = Robko01 $ \s deviceId -> do    
+cmdRobko cmd arg0 arg1 arg2 arg3 = Robko01 $ \s deviceId -> do
     let str = cmdString deviceId cmd arg0 arg1 arg2 arg3
     actual <- do
         send s str
         flush s
         threadDelay cmdTime
         recv s 255
-    
+
     return (actual, str)
 
 -- | Reset and Stop moving
 resetAndStop :: Robko01 ()
 resetAndStop = do
-    (response, shouldBe) <- cmdRobko 0 0 0 0 0 
-    
+    (response, shouldBe) <- cmdRobko 0 0 0 0 0
+
     let result = parseOnly parseStatus response
         parseStatus = string shouldBe
 
@@ -105,7 +107,7 @@ resetAndStop = do
 -- | Start of movement. Stopping is done by resetAndStop
 start :: Joint -> DriveDir -> DriveMode -> Int -> Robko01 ()
 start joint dir mode speed = do
-    (response, shouldBe) <- cmdRobko 1 (fromEnum joint) (fromEnum dir) (1 + fromEnum mode) speed 
+    (response, shouldBe) <- cmdRobko 1 (fromEnum joint) (fromEnum dir) (1 + fromEnum mode) speed
 
     let result = parseOnly parseStatus response
         parseStatus = string shouldBe
@@ -115,12 +117,12 @@ start joint dir mode speed = do
 -- | Get status of inputs
 getInputStatus :: Robko01 Int
 getInputStatus = do
-    (response, _) <- cmdRobko 2 0 0 0 0 
-    
+    (response, _) <- cmdRobko 2 0 0 0 0
+
     let result = parseOnly parseStatus response
         parseStatus = do
             string ":010200"
-            (x :: Int) <- read <$> count 8 digit    
+            (x :: Int) <- read <$> count 8 digit
             return x
 
     either fail return result
@@ -128,14 +130,14 @@ getInputStatus = do
 -- | Get status of joint
 getJointSteps :: Joint -> Robko01 JointSteps
 getJointSteps joint = do
-    (response,_) <- cmdRobko 8 (fromEnum joint) 0 0 0 
-    
+    (response,_) <- cmdRobko 8 (fromEnum joint) 0 0 0
+
     let result = parseOnly (parseStatus (fromEnum joint)) response
         parseStatus joint = do
             string ":0108"
             string . B.pack . show $ joint
             neg        <- parseBit
-            (x :: Int) <- read <$> count 8 digit    
+            (x :: Int) <- read <$> count 8 digit
             return . JointSteps $ if neg then -x else x
 
     either fail return result
