bf9cab60168fd57bd365e1445506e433a6825314
Vassil Keremidchiev <varosi@gmail.com>
2019-02-24 16:27:46 +0200

    Added another test.

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,5 +5,6 @@ import System.Hardware.Robko01
 main :: IO ()
 main = hspec $ do
     describe "cmdString" $ do
-        it "returns correct string for an example command" $ do
-            cmdString 9 8 1 2 3 4 `shouldBe` ":09081200030004\r\n"
+        it "returns correct strings for example commands" $ do
+            cmdString 9 8 1 2 3 4         `shouldBe` ":09081200030004\r\n"
+            cmdString 99 88 1 2 3333 4444 `shouldBe` ":99881233334444\r\n"
